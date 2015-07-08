4fb7896cf0ebf3cff071544e68b101420408641e
Isaac Chansky <ichansky@gmail.com>
2015-07-08 17:23:31 -0400

    add some usability stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,12 +1,63 @@
 /* jshint devel:true */
-console.log('\'Allo \'Allo!');
-
 
 $(document).ready(function(){
+  'use strict';
 
   var $container = $('.js-todo-container');
   var tpl = $('.js-todo-tpl').html();
-  var todos = JSON.parse( localStorage.getItem('todos') ) || [""];
+  var todos = JSON.parse( localStorage.getItem('todos') );
+
+  if(!todos || todos.length === 0) {
+    todos = [''];
+  }
+
+  function focusLast () {
+    $container.find('input').last().focus();
+  }
+
+  function addNew () {
+    $container.append(tpl);
+    $(document).trigger('todos:changed');
+  }
+
+  function deleteTodo (e) {
+    $(e.target).parent().remove();
+    $(document).trigger('todos:changed');
+  }
+
+  function saveTodos () {
+    var todos = $.map( $('.js-todo-text'), function(el){
+      return el.value;
+    });
+    todos = todos.filter(function(t){ return t.length > 0; });
+    localStorage.setItem('todos', JSON.stringify(todos) );
+  }
+
+  $(document).on('click', '.js-todo-add', addNew);
+
+  $(document).on('click', '.js-todo-delete', deleteTodo);
+
+  $(document).on('change', '.js-todo-text', saveTodos);
+
+  $container.on('keydown', 'input', function(e){
+    var key = e.which;
+    if( key === 13 && e.target.value.length !== 0) {
+      e.preventDefault();
+      addNew();
+      return;
+    }
+    if(key === 8 && e.target.value.length === 0) {
+      e.preventDefault();
+      deleteTodo(e);
+      return;
+    }
+  });
+
+
+  $(document).on('todos:changed', function(){
+    saveTodos();
+    focusLast();
+  });
 
   $.each(todos, function(i, todoText){
     var $tpl = $(tpl);
@@ -14,38 +65,6 @@ $(document).ready(function(){
     $container.append($tpl);
   });
 
-  function addNew () {
-    $container.append(tpl);
-    $container.find('input').last().focus();
-  }
 
-  $('.js-todo-add').on('click', addNew);
-
-  $('.js-todo-delete').on('click', function(e){
-    $(e.target).parent().remove();
-  })
-
-  $container.on('keypress', 'input', function(e){
-    var key = e.which;
-    if( key === 13) {
-      addNew();
-    }
-  })
-
-
-  $container.on('change', '.js-todo-text', function(){
-
-    var todos = $.map( $('.js-todo-text'), function(el){
-      return el.value;
-    });
-
-    todos = todos.filter(function(t){ return t.length > 0;})
-
-    console.log(todos);
-
-    localStorage.setItem('todos', JSON.stringify(todos) );
-
-  });
-
-})
+});
 
